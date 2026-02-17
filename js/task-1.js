2dfe220ff08cd0af5084c3516003d26af88234fb
nikuliia <iuliianiko@gmail.com>
2026-02-17 18:01:54 +0100

    commit

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const itemCategories = document.querySelectorAll('.js-item');
+console.log(`Number of categories: ${itemCategories.length}`);
+
+itemCategories.forEach(category => {
+  const title = category.querySelector('.js-heading').textContent;
+  const liTotal = category.querySelectorAll('.items-list > li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${liTotal}`);
+});
