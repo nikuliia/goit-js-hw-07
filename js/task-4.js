f64385f638ece6cb85dbeb74faed5edfa0285d1e
nikuliia <iuliianiko@gmail.com>
2026-02-19 15:44:22 +0100

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.js-form');
+const formBtn = document.querySelector('.js-btn');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const emailValue = form.elements.email.value.trim();
+  const passwordValue = form.elements.password.value.trim();
+
+  if (emailValue !== '' && passwordValue !== '') {
+    const formData = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(formData);
+  } else {
+    alert('All form fields must be filled in');
+  }
+
+  form.reset();
+});
