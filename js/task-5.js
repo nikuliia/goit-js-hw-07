8a99589b0af03cd38b67c7debf24408f8bc2d3a3
nikuliia <iuliianiko@gmail.com>
2026-02-19 17:50:17 +0100

    commit

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const btn = document.querySelector('.change-color');
+const bodyElem = document.querySelector('body');
+const spanElem = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bodyElem.style.backgroundColor = color;
+  spanElem.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
